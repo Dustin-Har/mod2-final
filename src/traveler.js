ff3ee7dd339349cfaaeeefd03a8184a26d8a6f2e
Dustin-Har <harbaughdustin@yahoo.com>
2021-04-22 14:20:19 -0400

    Add method to get trip information

--- src/traveler.js
+++ src/traveler.js
@@ -5,6 +5,15 @@ class Traveler {
     this.type = travelerInfo.travelerType;
     this.trips = this.getTripInfo(tripInfo, destinationInfo);
   }
+  
+  getTripInfo(tripInfo, destinationData) {
+    let allTripInfo = tripInfo.filter(trip => trip.userID === this.id);
+    allTripInfo.forEach(trip => {
+      const destinationIndex = destinationData.findIndex(destination => destination.id === trip.destinationID);
+      trip.destinationInfo = destinationData[destinationIndex];
+    });
+    return allTripInfo;
+  }
 
 }
 
