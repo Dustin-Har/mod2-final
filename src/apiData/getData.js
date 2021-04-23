2e316d9b9f8733df6186f55fa4c1bc8bf89081cb
Dustin-Har <harbaughdustin@yahoo.com>
2021-04-23 10:31:28 -0400

    Add fetch for travelers

--- src/apiData/getData.js
+++ src/apiData/getData.js
@@ -3,5 +3,9 @@ export const travelApi = () => {
     const message = err.message === 'Failed to fetch' ? 'Something went wrong. Please check your internet connection' : err.message;
   }
 
+  const travelersApi = fetch('http://localhost:3001/api/v1/travelers')
+  .then(response => response.json())
+  .catch(err => displayErrMessage(err));
+
 
 }
