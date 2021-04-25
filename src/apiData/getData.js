3ad9822a0554a75fc40d615ba527de4068574f70
Dustin-Har <harbaughdustin@yahoo.com>
2021-04-25 13:00:19 -0400

    Add single traveler fetch

--- src/apiData/getData.js
+++ src/apiData/getData.js
@@ -2,11 +2,17 @@ const displayErrMessage = (err) => {
   const message = err.message === 'Failed to fetch' ? 'Something went wrong. Please check your internet connection' : err.message;
 };
 
-const getTraveler = () => {
+const getTravelers = () => {
   return fetch('http://localhost:3001/api/v1/travelers')
   .then(response => response.json())
   .catch(err => displayErrMessage(err));
 };
+
+const getSingleTraveler = (id) => {
+  return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
+  .then(response => response.json())
+  .catch(err => displayErrMessage(err));
+}
 
 const getTrips = () => {
 return fetch('http://localhost:3001/api/v1/trips')
@@ -21,15 +27,16 @@ const getDestinations = () => {
 }
 
 const getAllApi = () => {
-  return Promise.all([getTraveler(), getTrips(), getDestinations()])
+  return Promise.all([getTravelers(),getSingleTraveler, getTrips(), getDestinations()])
   .then(data => {
     let apiData = {}
     apiData.traveler = data[0];
-    apiData.trips = data[1];
-    apiData.destinations = data[2];
+    apiData.singleTraveler = data[1];
+    apiData.trips = data[2];
+    apiData.destinations = data[3];
     return apiData;
   })
   .catch(err => displayErrMessage(err));
 };
 
-export {getTraveler, getTrips, getDestinations, getAllApi}
+export {getTravelers, getSingleTraveler, getTrips, getDestinations, getAllApi}
